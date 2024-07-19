1cd656e8bfb4d93196f4ce1457ccd66d9b8397ea
RK Labz <syed.rehan.kazmi@hotmail.com>
2024-07-19 22:45:19 +0500

    Add files via upload

---
+++ eMeterApp/WebAPIs/DataObjects/eMeterObjects.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace RestService.DataObjects
+{
+    public class eMeterObjects
+    {
+        [DataContract]
+        public class UserCredentials
+        {
+            [DataMember]
+            public string UserName { get; set; }
+
+            [DataMember]
+            public string Password { get; set; }
+        }
+
+        [DataContract]
+        public class ChangePasswordRequest : UserCredentials
+        {
+            [DataMember]
+            public string UName { get; set; }
+
+            [DataMember]
+            public string OldPassword { get; set; }
+
+            [DataMember]
+            public string NewPassword { get; set; }
+        }
+
+        [DataContract]
+        public class ResponseMessage
+        {
+            [DataMember]
+            public int Code { get; set; }
+
+            [DataMember]
+            public string Message { get; set; }
+        }
+
+        [DataContract]
+        public class Customer
+        {
+            [DataMember]
+            public string CustomerId { get; set; }
+
+            [DataMember]
+            public string Name { get; set; }
+
+            [DataMember]
+            public string Address { get; set; }
+        }
+
+        [DataContract]
+        public class Meter
+        {
+            [DataMember]
+            public string MeterId { get; set; }
+
+            [DataMember]
+            public string CustomerId { get; set; }
+        }
+
+        [DataContract]
+        public class MeterReading
+        {
+            [DataMember]
+            public string MeterId { get; set; }
+
+            [DataMember]
+            public DateTime ReadingDate { get; set; }
+
+            [DataMember]
+            public double ReadingValue { get; set; }
+        }
+
+        [DataContract]
+        public class User
+        {
+            [DataMember]
+            public int CustomerID { get; set; }
+
+            [DataMember]
+            public string FullName { get; set; }
+
+            [DataMember]
+            public string EmailAddress { get; set; }
+
+            [DataMember]
+            public string MobileNo { get; set; }
+
+            [DataMember]
+            public string CnicNo { get; set; }
+
+            [DataMember]
+            public string FullAddress { get; set; }
+
+            [DataMember]
+            public string CityName { get; set; }
+
+            [DataMember]
+            public bool ActiveInd { get; set; }
+
+            [DataMember]
+            public string CreatedBy { get; set; }
+
+            [DataMember]
+            public DateTime CreatedAt { get; set; }
+        }
+
+        [DataContract]
+        public class Credentials
+        {
+            [DataMember]
+            public string UserName { get; set; }
+
+            [DataMember]
+            public string Password { get; set; }
+
+            [DataMember]
+            public int CustomerID { get; set; }
+
+            [DataMember]
+            public bool ActiveInd { get; set; }
+
+            [DataMember]
+            public DateTime ActiveDate { get; set; }
+
+            [DataMember]
+            public string CreatedBy { get; set; }
+
+            [DataMember]
+            public DateTime CreatedAt { get; set; }
+
+        }
+
+        [DataContract]
+        public class MeterType
+        {
+            [DataMember]
+            public int TypeID { get; set; }
+
+            [DataMember]
+            public string TypeName { get; set; }
+
+            [DataMember]
+            public bool ActiveInd { get; set; }
+
+            [DataMember]
+            public string CreatedBy { get; set; }
+
+            [DataMember]
+            public DateTime CreatedAt { get; set; }
+        }
+
+        [DataContract]
+        public class MeterDetails
+        {
+            [DataMember]
+            public int MeterID { get; set; }
+
+            [DataMember]
+            public int CustomerID { get; set; }
+
+            [DataMember]
+            public string MeterNo { get; set; }
+
+            [DataMember]
+            public string RefNo { get; set; }
+
+            [DataMember]
+            public string OldRefNo { get; set; }
+
+            [DataMember]
+            public DateTime ConnectionDate { get; set; }
+
+            [DataMember]
+            public int StatusID { get; set; }
+
+            [DataMember]
+            public int MeterLoad { get; set; }
+
+            [DataMember]
+            public bool ActiveInd { get; set; }
+
+            [DataMember]
+            public string CreatedBy { get; set; }
+
+            [DataMember]
+            public DateTime CreatedAt { get; set; }
+        }
+        [DataContract]
+        public class RecordReadingRequest
+        {
+            [DataMember]
+            public BillMain BillMain { get; set; }
+
+            [DataMember]
+            public List<BillDetails> BillDetailsList { get; set; }
+        }
+
+        [DataContract]
+        public class BillMain
+        {
+            [DataMember]
+            public int MeterID { get; set; }
+
+            [DataMember]
+            public int CustomerID { get; set; }
+
+            [DataMember]
+            public DateTime IssueDate { get; set; }
+
+            [DataMember]
+            public DateTime DueDate { get; set; }
+
+            [DataMember]
+            public int StatusID { get; set; }
+
+            [DataMember]
+            public DateTime BillMonth { get; set; }
+
+            [DataMember]
+            public DateTime ReadingDate { get; set; }
+
+            [DataMember]
+            public int UnitsConsumed { get; set; }
+
+            [DataMember]
+            public string CreatedBy { get; set; }
+
+            [DataMember]
+            public DateTime CreatedAt { get; set; }
+        }
+
+        [DataContract]
+        public class BillDetails
+        {
+            [DataMember]
+            public int BillID { get; set; }
+
+            [DataMember]
+            public int SlabID { get; set; }
+
+            [DataMember]
+            public decimal SlabRate { get; set; }
+
+            [DataMember]
+            public int UnitsApplied { get; set; }
+
+            [DataMember]
+            public decimal SlabAmount { get; set; }
+        }
+
+    }
+}
---
+++ eMeterApp/WebAPIs/IeMeterAPI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Threading.Tasks;
+using static RestService.DataObjects.eMeterObjects;
+
+namespace RestService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IeMeterAPI" in both code and config file together.
+    [ServiceContract]
+    public interface IeMeterAPI
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/SignIn", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        ResponseMessage SignIn(UserCredentials credentials);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/SignOut", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        ResponseMessage SignOut(UserCredentials credentials);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/ChangePassword", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        ResponseMessage ChangePassword(ChangePasswordRequest request);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/RegisterCustomer", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        ResponseMessage RegisterCustomer(User user);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/AddMeter", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        ResponseMessage AddMeter(MeterType meter);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "RecordReading", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped)]
+        ResponseMessage RecordReading(RecordReadingRequest request);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/GetAllUsers", ResponseFormat = WebMessageFormat.Json)]
+        List<User> GetAllUsers();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/CreateCredentials", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        ResponseMessage CreateCredentials(Credentials credentials);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/RegisterMeterWithCustomer", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        ResponseMessage RegisterMeterWithCustomer(MeterDetails meterDetails);
+    }
+}
